59c749cf0d054933d58d179244ea2aa26302b07d
sultan <sultanshah.bscsf24@gmail.com>
2026-02-14 17:33:25 +0500

    Add UI fucntionality
    Added interactive Visuals and changed game logic

--- javascript.js
+++ javascript.js
@@ -1,96 +1,130 @@
 console.log("Hello, World!");
 
-let humanScore=0;
+let playerScore=0;
 let computerScore=0;
+let rockButton=document.querySelector(".rock");
+
+let paperButton=document.querySelector(".paper");
+
+let scissorButton=document.querySelector(".scissors");
+
+rockButton.addEventListener('click',playRound);
+
+paperButton.addEventListener('click',playRound);
+
+scissorButton.addEventListener('click',playRound);
+let wingMsg=document.getElementById("win-msg");
+let computerScoreMsg=document.getElementById("computer-score");
+let playerScoreMsg=document.getElementById("player-score");
 
 function getComputerChoice()
 {
     let val=(Math.floor((Math.random()*10)))%3 ;
-    if(val===0)
-        return "rock";
-    else if(val===1)
-        return "paper";
-
-    else if(val===2)
-        return "scissors";
-}
-
-function getHumanChoice()
-{
-    return prompt("Choose Rock Paper Scissors");
+    return val;
 }
 
 
-
-function playRound()
+function updateScoreBoard()
 {
-    console.log("ENTER YOUR CHOICE");
-    let humanChoice=getHumanChoice();
-    let computerChoice=getComputerChoice();
+    computerScoreMsg.textContent=`COMPUTER SCORE :${computerScore}`;
+    playerScoreMsg.textContent=`PLAYER SCORE :${playerScore}`;
+}
+function increaseComputerScore()
+{
+    computerScore++;
+}
+function increasePlayerScore()
+{
+    playerScore++;
+}
+function declarePlayerWinner()
+{
+    wingMsg.textContent=`YOU WON BRUH/GAL`;
+}
+function declareComputerWinner()
+{
+    wingMsg.textContent=`COMPUTER WON`;
+}
+function showTie()
+{
+    wingMsg.textContent=`TIE ~`;
+}
 
-    if(humanChoice=="rock" || humanChoice=="ROCK")
-    {
-        if(computerChoice=="rock" )
-            console.log("No one wins Tie!");
-        else if(computerChoice=="paper" )
-        {
-            console.log("Computer Wins");
-            computerScore++;
-        }
-        else if (computerChoice=="scissors" )
-        {
-            console.log("Player Won")
-            humanScore++;
-        }
-    }
-    else if(humanChoice=="paper" || humanChoice == "PAPER")
-    {
-        if(computerChoice=="rock")
-            {
-                console.log("Human Wins");
-                humanScore++;
-            }
-        else if(computerChoice=="paper")
-        {
-            console.log("No-one Wins Tie");
-        }
-        else if (computerChoice=="scissors" )
-        {
-            console.log("Computer Wins")
-            computerScore++;
-        }
 
-    }
-    else if(humanChoice=="scissors" || humanChoice=="SCISSORS")
+function updateStatsForComputer()
+{
+    declareComputerWinner();
+    increaseComputerScore();
+    updateScoreBoard();
+}
+
+function updateStatForPlayer()
+{
+    declarePlayerWinner();
+    increasePlayerScore();
+    updateScoreBoard();
+}
+
+
+function playRound(e)
+{
+    humanChoice=e.target.parentElement;
+    console.log(humanChoice);
+    console.log(rockButton);
+    computerChoice=getComputerChoice();
+    if(humanChoice==rockButton)
     {
-         if(computerChoice=="rock" )
-            {
-                console.log("Computer Wins");
-                computerScore++;
-            }
-        else if(computerChoice=="paper" )
+        if(computerChoice==0)
         {
-            console.log("Human Wins!");
-            humanScore++;
+           showTie();
         }
-        else if (computerChoice=="scissors" )
+        else if(computerChoice==1)
+        {   
+            updateStatsForComputer();
+        }
+        else if(computerChoice==2)
         {
-            console.log("No one wins Tie");
-            
+            updateStatForPlayer();
         }
-        
     }
-    return 1;
-    
+    else if(humanChoice==paperButton)
+    {
+        if(computerChoice==0)
+        {
+            updateStatForPlayer();
+        }
+        else if(computerChoice==1)
+        {
+            showTie();
+        }
+        else if(computerChoice==2)
+        {
+            updateStatsForComputer();   
+        }
+    }
+    else if(humanChoice==scissorButton)
+    {
+        if(computerChoice==0)
+        {
+            showTie();
+        }
+        else if(computerChoice==1)
+        {
+            updateStatForPlayer();
+        }
+        else if(compuerChoice==2)
+        {
+            updateStatsForComputer();
+        }
+    }
+    else
+    {
+        console.log("ERROR");
+    }
+
+
  
 }
-let rounds=0;
-while(rounds<5)
-{
-    rounds+=playRound();
-}
 
-console.log("Game Summary");
-console.log("COMPUTER SCORE : "+ computerScore);
-console.log("HUMAN SCORE " + humanScore);
+
 
